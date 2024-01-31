72814c994743c6139809d0199f327ec85cda7a5d
YanDz <155983383+YanisZedira@users.noreply.github.com>
2024-01-31 19:40:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from data_preparation import DataPreparation
-from additif import Additif
+from Additif import AdditifModel
+
+csv_path = r"C:\Users\pc\Desktop\TP HETIC\MathsIA\Additif\vente_maillots_de_bain(1).csv"
+data_preparation_object = DataPreparation(csv_path)
+regression_object = AdditifModel(data_preparation_object)
+regression_object.show_model_predictions()
 
 
-csv_path = r"C:\Users\yzedira\Desktop\pypy\vente_maillots_de_bain(1).csv"
-data_preparation_object = DataPreparation(csv_path) 
-regression_object = Additif(data_preparation_object)
 
-data_preparation_object.show_graph()
+#YANIS, AYMEN DIA1
